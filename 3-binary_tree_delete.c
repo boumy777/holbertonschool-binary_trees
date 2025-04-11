cd07c4921af8342fb1178024e7af79b111ca572f
root <brandondialuta@gmail.com>
2025-04-11 12:55:18 +0200

    function that deletes an entire binary tree

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -3,11 +3,12 @@
 
 /**
  * binary_tree_delete - Supprime un arbre binaire entier
- * @tree: Pointeur vers la racine de l'arbre à supprimer
+ * @tree: pointeur vers la racine de l'arbre à supprimer
  *
- * Cette fonction libère récursivement tous les nœuds de l'arbre.
- * Si tree est NULL, elle ne fait rien.
+ * Cette fonction libère récursivement tous les nœuds de l'arbre binaire.
+ * Si @tree est NULL, rien n'est fait.
  */
+
 void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree == NULL)
