db73644fadc53b73c91e38305e8c1fd977cec5a4
root <brandondialuta@gmail.com>
2025-04-13 19:52:33 +0200

    function that checks if a given node is a root

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_inorder - Parcourt un arbre binaire en ordre infixe
+ * @tree: Pointeur vers la racine de l'arbre à parcourir
+ * @func: Pointeur vers une fonction à appeler pour chaque nœud.
+ *        La valeur contenue dans le nœud est passée en paramètre.
+ *
+ * Description:
+ * Si tree ou func est NULL, ne rien faire.
+ */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
+}
+
